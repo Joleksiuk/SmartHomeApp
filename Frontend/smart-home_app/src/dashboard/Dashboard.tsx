840cd345b51ed7ffde33c06d929ed8866165b7cf
Joleksiuk <julkaoleksiuk26@gmail.com>
2022-11-30 00:50:08 +0100

    Testing tuya controller

--- Frontend/smart-home_app/src/dashboard/Dashboard.tsx
+++ Frontend/smart-home_app/src/dashboard/Dashboard.tsx
@@ -82,7 +82,7 @@ const toggle=()=>{
   ShellyController.toggleLight()
 }
 const testTuya=()=>{
-  TuyaService.testTuyaConnection
+  TuyaService.testTuyaConnection()
 }
 
 const mdTheme = createTheme();
